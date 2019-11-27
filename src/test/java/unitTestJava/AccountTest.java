9f96b8267fa5487aa496eb68a84d048bd37e9f52
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-11-27 15:20:22 +0100

    Test Update

--- src/test/java/unitTestJava/AccountTest.java
+++ src/test/java/unitTestJava/AccountTest.java
@@ -2,6 +2,7 @@ package unitTestJava;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class AccountTest {
@@ -10,6 +11,15 @@ public class AccountTest {
     public void myTest(){
         Account newAccount = new Account();
         assertFalse(newAccount.isActive(), "Check if new account is not active");
+//        assertFalse(true, "Check if new account is not active");
+//        assertFalse(true);
+    }
 
+    @Test
+    void myTest2(){
+        Account newAccount = new Account();
+        assertFalse(newAccount.isActive());
+        newAccount.activate();
+        assertTrue(newAccount.isActive());
     }
 }
