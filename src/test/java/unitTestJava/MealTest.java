ad786c9a18f418ac41f1cd950d9058c02e656fed
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-11-28 20:04:48 +0100

    Test Update

--- src/test/java/unitTestJava/MealTest.java
+++ src/test/java/unitTestJava/MealTest.java
@@ -9,5 +9,33 @@ class MealTest {
     @Test
     void shouldReturnDiscountedPrice() {
 
+        //given
+        Meal meal = new Meal(35);
+
+        //when
+        int discountedPrice = meal.getDiscountedPrice(7);
+
+        //then
+        assertEquals(25, discountedPrice);
+    }
+
+    @Test
+    void referenceToTheSameObjectShouldBeEquals(){
+        //give
+        Meal meal1 = new Meal(10);
+        Meal meal2 = meal1;
+
+        //then
+        assertSame(meal1, meal2);
+    }
+
+    @Test
+    void referenceToDifferentObjectShouldNotBeEquals(){
+        //give
+        Meal meal1 = new Meal(10);
+        Meal meal2 = new Meal(20);
+
+        //then
+        assertSame(meal1, meal2);
     }
 }
