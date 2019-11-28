f1f2c4e8d5435b0536ca55f3b7cef5cc8ab2d9ab
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-11-28 19:58:02 +0100

    Test Update

---
+++ src/test/java/unitTestJava/MealTest.java
@@ -0,0 +1,13 @@
+package unitTestJava;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MealTest {
+
+    @Test
+    void shouldReturnDiscountedPrice() {
+
+    }
+}
