2b2877585268fbf4d865281a28d13307954fbedd
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-12-07 16:40:02 +0100

    Test Update

--- src/test/java/unitTestJava/OrderBackupTest.java
+++ src/test/java/unitTestJava/OrderBackupTest.java
@@ -1,4 +1,27 @@
 package unitTestJava;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class OrderBackupTest {
+
+    private static OrderBeckup orderBeckup;
+
+    @BeforeAll
+    static void  setup() throws FileNotFoundException {
+        orderBeckup = new OrderBeckup();
+        orderBeckup.createFile();
+    }
+
+    @Test
+    void
+
+    @AfterAll
+    static void tearDown() throws IOException {
+        orderBeckup.closeFile();
+    }
 }
