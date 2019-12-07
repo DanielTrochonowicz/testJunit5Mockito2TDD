66c07358fc0a8313bbf389f3dc1a5c1624b70b8c
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-12-07 16:36:56 +0100

    Test Update

---
+++ src/main/java/unitTestJava/OrderBeckup.java
@@ -0,0 +1,24 @@
+package unitTestJava;
+
+import java.io.*;
+
+public class OrderBeckup {
+
+    private Writer writer;
+
+    public Writer getWriter() {
+        return writer;
+    }
+    void createFile() throws FileNotFoundException {
+        File file = new File("orderBackup.txt");
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+        writer = new BufferedWriter(outputStreamWriter);
+    }
+    void backupOrder(Order order) throws IOException {
+        writer.append(order.toString());
+    }
+    void closeFile() throws IOException {
+        writer.close();
+    }
+}
