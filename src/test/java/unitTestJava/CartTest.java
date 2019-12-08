91e03058027b21e7a36b83881659ce608936370e
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-12-08 15:42:56 +0100

    Test Update

--- src/test/java/unitTestJava/CartTest.java
+++ src/test/java/unitTestJava/CartTest.java
@@ -1,14 +1,19 @@
 package unitTestJava;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Test cases for Cart")
 class CartTest {
 
+   // @Disabled
     @Test
+    @DisplayName("Cart is able to process 1000 orders in 100 ms")
     void simulateLeargeOrder() {
 
         //given
@@ -18,4 +23,5 @@ class CartTest {
         //then
         assertTimeout(Duration.ofMillis(100), cart::simulateLeargeOrder);
     }
+
 }
