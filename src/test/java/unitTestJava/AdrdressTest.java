e59ba44453f808178a2b1c9ff6191cd63f9a2dd8
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-12-15 07:32:33 +0100

    Test Update

---
+++ src/test/java/unitTestJava/AdrdressTest.java
@@ -0,0 +1,31 @@
+package unitTestJava;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.lessThan;
+
+public class AdrdressTest {
+
+    @ParameterizedTest
+    @CsvSource({"Powstańców, 10", "Armii Krajowej,  57/11", "'Romka, Tomka, Atomka', 40"})
+    void givenAddressesShouldNotBeEmptyAndHaveProperNames(String street, String number) {
+        assertThat(street, notNullValue());
+        assertThat(street, containsString("a"));
+        assertThat(number, notNullValue());
+        assertThat(number.length(), lessThan(8));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/addresses.csv")
+    void AddressesFromFileShouldNotBeEmptyAndHaveProperNames(String street, String number) {
+        assertThat(street, notNullValue());
+        assertThat(street, containsString("a"));
+        assertThat(number, notNullValue());
+        assertThat(number.length(), lessThan(8));
+    }
+}
