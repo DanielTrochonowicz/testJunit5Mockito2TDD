6e566dc416fd99a172f02515129edd2d496e3e57
Daniel Trochonowicz <trochonowiczdaniel@wp.pl>
2019-12-31 21:57:18 +0100

    Test Update

---
+++ src/test/java/unitTestJava/account/AccountRepositoryStub.java
@@ -0,0 +1,21 @@
+package unitTestJava.account;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class AccountRepositoryStub implements AccountRepository {
+    @Override
+    public List<Account> getAllAccounts() {
+
+        Address address1 = new Address("Kwiatki", "10/3");
+        Account account1 = new Account(address1);
+
+        Address address2 = new Address("Polna", "1/2");
+        Account account2 = new Account();
+
+        Account account3 = new Account(address2);
+        return Arrays.asList(account1,account2,account3);
+    }
+
+}
--- src/test/java/unitTestJava/account/AccountServiceTest.java
+++ src/test/java/unitTestJava/account/AccountServiceTest.java
@@ -2,12 +2,24 @@ package unitTestJava.account;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+
 public class AccountServiceTest {
 
     @Test
     void getAllActiveAccounts(){
 
         //given
-        AccountService accountService = new AccountService()
+        AccountRepository accountRepositoryStub = new AccountRepositoryStub();
+        AccountService accountService = new AccountService(accountRepositoryStub);
+
+        //when
+        List<Account> accountList = accountService.getAllActiveAccounts();
+
+        //then
+        assertThat(accountList, hasSize(2));
     }
 }
